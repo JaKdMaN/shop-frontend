01867c8d0ff4c15ad3686cdb4b0974a4ad4ade44
Максим <jakdman2003@gmail.com>
2023-11-11 03:07:51 +0300

    Initial commit

---
+++ src/router/index.ts
@@ -0,0 +1,18 @@
+import { route } from 'quasar/wrappers';
+import {
+  createRouter,
+  createWebHistory,
+} from 'vue-router';
+import routes from './routes';
+
+const router = createRouter({
+  scrollBehavior: () => ({ left: 0, top: 0 }),
+  routes,
+
+  // Leave this as is and make changes in quasar.conf.js instead!
+  // quasar.conf.js -> build -> vueRouterMode
+  // quasar.conf.js -> build -> publicPath
+  history: createWebHistory(process.env.VUE_ROUTER_BASE),
+})
+
+export default router
---
+++ src/router/routes.ts
@@ -0,0 +1,17 @@
+import { RouteRecordRaw } from 'vue-router';
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'app',
+    component: () => import('layouts/MainLayout.vue'),
+  },
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/NotFoundPage.vue'),
+  },
+];
+
+export default routes
